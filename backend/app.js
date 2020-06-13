627843c8333a432ad9dbb0a7a712ef3a85f3bc7d
Samantha Jimenez <samantha@Samanthas-MacBook-Pro.local>
2020-06-12 23:19:48 -0400

    complete app.js backend

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv').config();
-const { checkFirebaseToken } = require('./middleware/auth');
+// const { checkFirebaseToken } = require('./middleware/auth');
 const PORT = process.env.PORT;
 const app = express();
 
@@ -12,15 +12,15 @@ app.use(bodyParser.json());
 
 // const port = 3005; 
 
-const usersRouter = require('./routes/Users/Users');
-const photosRouter = require('./routes/Photos/Photos');
+const usersRouter = require('./routes/Users');
+// const photosRouter = require('./routes/Photos');
 // const hashtagsRouter = require('./routes/Hashtags/Hashtags')
 // const uploadRouter = require('./routes/Uploads/Uploads');
 // const likesRouter = require('./routes/Likes/Likes');
 
 
 app.use('/users', usersRouter);
-app.use('/photos', photosRouter);
+// app.use('/photos', photosRouter);
 // app.use('/hashtags', hashtagsRouter);
 // app.use('/posts/uploads', uploadRouter)
 // app.use('/likes', likesRouter);
