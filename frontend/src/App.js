9a96d4197538f8d84f4107e5fb349218972461f6
Samantha Jimenez <samantha@Samanthas-MacBook-Pro.local>
2020-06-12 23:43:48 -0400

    sign up and login route path in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import LogIn from './components/LogIn';
+import SignUp from './components/SignUp';
+import Feed from './components/Feed';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Switch> */}
+          <Route exact path={"/"}>
+            <LogIn/>
+          </Route>
+          <Route exact path={"/signup"}>
+            <SignUp/>
+          </Route>
+          {/* <Route exact path={"/home"}>
+            <Feed/>
+          </Route> */}
+          {/* <Route exact path={"/upload"}>
+            <Upload/>
+          </Route> */}
+          {/* <Route exact path={"/profile"}>
+            <Profile/>
+          </Route> */}
+        {/* </Switch> */}
     </div>
   );
 }
