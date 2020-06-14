67e7b0fe32e14ccff38a0ee515eb8926e23734b3
Samantha Jimenez <samantha@Samanthas-MacBook-Pro.local>
2020-06-13 22:17:24 -0400

    create login css

--- frontend/src/components/LogIn.js
+++ frontend/src/components/LogIn.js
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 import { NavLink, useHistory } from "react-router-dom";
 import { login } from '../util/firebaseFunctions';
+import '../css/LogIn.css';
+import ig_logo from '../ImgFiles/ig_logo.png';
+import appStore_logo from '../ImgFiles/appStoreIcon.png';
+import googlePlay_logo from '../ImgFiles/googlePlayIcon.png';
 
 export default function LogIn() {
   const [email, setEmail] = useState("");
@@ -40,7 +44,7 @@ export default function LogIn() {
     <div className="mainDiv">
             <div className="logIn">
                 <div className="topDiv">
-                    {/* <img className='igImg_login' src={ig_logo} alt='instagram_logo'/> */}
+                    <img className='igImg_login' src={ig_logo} alt='instagram_logo'/>
                     <form className="logInForm" onSubmit={handleSubmit}>
                         <input className="login_input" type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} value={email} />
                         <input className="login_input" type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} value={password} autoComplete="on" />
@@ -56,8 +60,8 @@ export default function LogIn() {
                 <div className="bottomDiv">
                     <p className="bottomP">Get the app.</p>
                     <div className="appLinks">
-                        {/* <img className='appStore_login' src={appStore_logo} alt='appStore_logo'/> */}
-                        {/* <img className='googlePlay_login' src={googlePlay_logo} alt='googlePlay_logo'/> */}
+                        <img className='appStore_login' src={appStore_logo} alt='appStore_logo'/>
+                        <img className='googlePlay_login' src={googlePlay_logo} alt='googlePlay_logo'/>
                     </div>
                 </div>
             </div>
