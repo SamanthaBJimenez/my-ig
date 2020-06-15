7a8aa4b6cefd7e80e156347e615ad7fb7df9773d
Samantha Jimenez <samantha@Samanthas-MacBook-Pro.local>
2020-06-15 17:53:57 -0400

    a

--- backend/routes/Users.js
+++ backend/routes/Users.js
@@ -2,7 +2,7 @@ const users = require('express').Router();
 const {getAllUsers, createNewUser, getSingleUserById, getSingleUserByEmail} = require('../queries/Users');
 const { checkFirebaseToken } = require('../middleware/auth');
 
-users.get('/', checkFirebaseToken, getAllUsers);
+users.get('/', /*checkFirebaseToken,*/ getAllUsers);
 users.post('/', createNewUser);
 users.get('/id/:id', getSingleUserById)
 users.get('/email/:email', getSingleUserByEmail);
