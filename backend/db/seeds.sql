3154e7a3d2c8fd07c0cd3e028ffa92ff67ec3734
Samantha Jimenez <samanthajimenez@pursuit.org>
2020-09-26 17:37:05 -0400

    add seed data

---
+++ backend/db/seeds.sql
@@ -0,0 +1,5 @@
+\c my_ig_db;
+
+
+INSERT INTO USERS(id, username, password, full_name, bio, email, avatar)
+    VALUES('gMfw4cWqRMNmPNnEZTbHSlVY72q1', 'Jonny', 'jonny123', 'Jonny Boy', 'Hey, Welcome!', 'jonny@gmail.com', null);
